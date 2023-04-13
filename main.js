b11c54fb4c346d24742c0fc2d1cbc8201a4dba0d
Jeis Caldera <jeismarycaldera@gmail.com>
2023-04-12 21:09:52 -0300

    Se integro carrito de compra con menu de la compra

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burgerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const shoppingCartMenu= document.querySelector(".navbar-shopping-cart");
+const productDetail = document.querySelector(".product-detail")
+const isProductDetailCart = productDetail.classList.contains('inactive');
+
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+burgerMenu.addEventListener('click',toggleMobileMenu);
+shoppingCartMenu.addEventListener('click', toggleProductDetailCart);
 
 function toggleDesktopMenu() {
-   console.log('Click');
+    if (!isProductDetailCart){
+        productDetail.classList.add('inactive');
+     }
     desktopMenu.classList.toggle('inactive');
 }
+ 
+function toggleMobileMenu(){
+    
+     
+     if (!isProductDetailCart){
+        productDetail.classList.add('inactive');
+     }
+     mobileMenu.classList.toggle('inactive');
+}
 
+function toggleProductDetailCart(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClose = desktopMenu.classList.contains('inactive')
+    
+    if(!isDesktopMenuClose){
+        desktopMenu.classList.add('inactive')
+    }
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    productDetail.classList.toggle('inactive');
+}
